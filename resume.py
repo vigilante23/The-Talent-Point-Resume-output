6f22608f0f7134578f3d0dbf4ff68587a5e642ab
vigilante23 <50033762+vigilante23@users.noreply.github.com>
2023-08-08 15:05:23 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import os
+import re
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.chains import RetrievalQA
+
+
 st.write("hello world")
          
