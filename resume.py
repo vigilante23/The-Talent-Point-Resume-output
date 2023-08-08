ab4c595dcf3096633e711873c3ea721a9fe10dc0
vigilante23 <50033762+vigilante23@users.noreply.github.com>
2023-08-08 15:46:31 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import re
+import openai
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -9,9 +10,31 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from langchain.chains import RetrievalQA
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
-st.write('hello')
-              
+st.set_page_config(page_title="CHECK DETAILS FROM YOUR RESUME")
+st.header("Find the Right Talent for Your Business")
+
+pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+
+embeddings = OpenAIEmbeddings()
+knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+st.write(knowledge_base)
         
          
