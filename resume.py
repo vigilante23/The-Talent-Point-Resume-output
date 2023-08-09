44ee8c28d8c68e1bb7045022c121566d4b6bae07
vigilante23 <50033762+vigilante23@users.noreply.github.com>
2023-08-09 11:45:00 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -38,7 +38,7 @@ if pdf is not None:
       embeddings = OpenAIEmbeddings()
       knowledge_base = FAISS.from_texts(chunks, embeddings)
       
-      query = "Please provide the following candidate details: Name, Email, Phone Number, Education, Experience. Please format each detail on a new line."
+      query = "Please provide the following candidate details: Name, Email, Phone Number, Education, Experience, skills, DOB. Please provide each data in new line so that easy to understand and also add heading infront of that record."
       qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=knowledge_base.as_retriever())
       paragraph = qa.run(query)
       st.write(paragraph)
